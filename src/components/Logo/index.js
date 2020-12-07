08177bf71aa16ddc0e5949638e2b086ac3b14fd9
MK KNIGHT <mk.knight970@gmail.com>
2020-12-07 11:53:42 +0530

    New Commits

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 /**
-* @author
-* @function Logo
-**/
+ * @author
+ * @function Logo
+ **/
 
 const Logo = (props) => {
-  return(
+  return (
     <div className="logo">
-        <a href="#">Rizwan Khan</a>
+      <a href="#">MK KNIGHT</a>
     </div>
-   )
+  );
+};
 
- }
-
-export default Logo
+export default Logo;
